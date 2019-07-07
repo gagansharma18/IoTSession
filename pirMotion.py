e97a7e058c5f6745b14f862fa149d77032cd34a6
gagansharma18 <gagan.sharma28@gmail.com>
2019-07-07 19:43:28 +0530

    Add all relay on and off

--- pirMotion.py
+++ pirMotion.py
@@ -4,9 +4,11 @@ import time
 sensor = 4
 relay = 5
 stayOnTime = 3 #seconds
+SleepTime = 1
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(sensor, GPIO.IN, GPIO.PUD_DOWN)
+pinList = [5,6,13,19,26,16,20,21]
 
 previous_state = False
 current_state = False
@@ -18,12 +20,20 @@ while True:
     if current_state != previous_state:
         new_state = "HIGH" if current_state else "LOW"
         if current_state:
-            GPIO.setup(relay, GPIO.OUT)
-            GPIO.output(relay, GPIO.LOW) # it means relay on
+            # GPIO.setup(relay, GPIO.OUT)
+            # GPIO.output(relay, GPIO.LOW) # it means relay on
             print("MOTION DETECTED! RELAY IS ON")
-            time.sleep(stayOnTime)
+            # time.sleep(stayOnTime)
+            for i in pinList: 
+                GPIO.setup(i, GPIO.OUT) 
+                GPIO.output(i, GPIO.LOW)
+                time.sleep(SleepTime); 
         else:
-            GPIO.setup(relay, GPIO.OUT)
-            GPIO.output(relay, GPIO.HIGH) # it means rlay off
+            # GPIO.setup(relay, GPIO.OUT)
+            # GPIO.output(relay, GPIO.HIGH) # it means rlay off
             print("NO MOTION IN %s SECONDS SO RELAY IS OFF" % (stayOnTime))
+            for i in pinList: 
+                GPIO.setup(i, GPIO.OUT) 
+                GPIO.output(i, GPIO.HIGH)
+                time.sleep(SleepTime); 
         #print("GPIO pin %s is %s current state %s" % (sensor, new_state, current_state))
