33878df04c1e2b6f64dcb79e53779a28c68b5e40
gagansharma18 <gagan.sharma28@gmail.com>
2019-07-07 18:38:35 +0530

    increase stay on time

--- pirMotion.py
+++ pirMotion.py
@@ -3,6 +3,7 @@ import time
 
 sensor = 4
 relay = 5
+stayOnTime = 3 #seconds
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(sensor, GPIO.IN, GPIO.PUD_DOWN)
@@ -11,15 +12,16 @@ previous_state = False
 current_state = False
 
 while True:
-    time.sleep(1)
+    time.sleep(0.1)
     previous_state = current_state
     current_state = GPIO.input(sensor)
     if current_state != previous_state:
-        new_state = "LOW" if current_state else "HIGH"
+        new_state = "HIGH" if current_state else "LOW"
         if current_state:
             GPIO.setup(relay, GPIO.OUT)
-            GPIO.output(relay, GPIO.LOW)
+            GPIO.output(relay, GPIO.LOW) # it means relay on
+            time.sleep(stayOnTime)
         else:
             GPIO.setup(relay, GPIO.OUT)
-            GPIO.output(relay, GPIO.HIGH)
+            GPIO.output(relay, GPIO.HIGH) # it means rlay off
         print("GPIO pin %s is %s current state %s" % (sensor, new_state, current_state))
