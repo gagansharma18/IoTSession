3e1b7a387bd1c7b57785c9b889ebc20f3aa1c857
gagansharma18 <gagan.sharma28@gmail.com>
2019-07-07 18:21:34 +0530

    updat sleep time

--- pirMotion.py
+++ pirMotion.py
@@ -11,7 +11,6 @@ previous_state = False
 current_state = False
 
 while True:
-    time.sleep(1)
     previous_state = current_state
     current_state = GPIO.input(sensor)
     if current_state != previous_state:
@@ -20,6 +19,7 @@ while True:
             GPIO.setup(relay, GPIO.OUT)
             GPIO.output(relay, GPIO.LOW)
         else:
+            time.sleep(1)
             GPIO.setup(relay, GPIO.OUT)
             GPIO.output(relay, GPIO.HIGH)
         print("GPIO pin %s is %s current state %s" % (sensor, new_state, current_state))
