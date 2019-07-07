1252aff0d9924aa2de07d6e2a46032452ac6b062
gagansharma18 <gagan.sharma28@gmail.com>
2019-07-07 17:56:12 +0530

    Update pirMotion.py
    fix indent

--- pirMotion.py
+++ pirMotion.py
@@ -7,13 +7,16 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(PIR_PIN, GPIO.IN)         #Read output from PIR motion sensor
 GPIO.setup(RELAY_PIN, GPIO.OUT)         #RELAY output pin
+
 while True:
-i=GPIO.input(PIR_PIN)
-if i==0:                 #When output from motion sensor is LOW
-print "No intruders",i
-GPIO.output(RELAY_PIN, 0)  #Turn OFF RELAY
-time.sleep(0.1)
-elif i==1:               #When output from motion sensor is HIGH
-print "Intruder detected",i
-GPIO.output(RELAY_PIN, 1)  #Turn ON RELAY
-time.sleep(0.1)
+    i=GPIO.input(PIR_PIN)
+
+    if i==0:                 #When output from motion sensor is LOW
+        print "No intruders",i
+        GPIO.output(RELAY_PIN, 0)  #Turn OFF RELAY
+        time.sleep(0.1)
+
+    elif i==1:               #When output from motion sensor is HIGH
+        print "Intruder detected",i
+        GPIO.output(RELAY_PIN, 1)  #Turn ON RELAY
+        time.sleep(0.1)
