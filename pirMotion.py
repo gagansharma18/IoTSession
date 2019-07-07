ecb54a78ecd2cc104645f05cab9881a1e0498ce4
gagansharma18 <gagan.sharma28@gmail.com>
2019-07-07 18:24:22 +0530

    Update pirMotion.py

--- pirMotion.py
+++ pirMotion.py
@@ -11,15 +11,15 @@ previous_state = False
 current_state = False
 
 while True:
+    time.sleep(1)
     previous_state = current_state
     current_state = GPIO.input(sensor)
     if current_state != previous_state:
-        new_state = "HIGH" if current_state else "LOW"
+        new_state = "LOW" if current_state else "HIGH"
         if current_state:
             GPIO.setup(relay, GPIO.OUT)
             GPIO.output(relay, GPIO.LOW)
         else:
-            time.sleep(1)
             GPIO.setup(relay, GPIO.OUT)
             GPIO.output(relay, GPIO.HIGH)
         print("GPIO pin %s is %s current state %s" % (sensor, new_state, current_state))
